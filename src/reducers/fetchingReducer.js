8cea10600b6928fb82b5adb60aeed6c5ae5c7005
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-19 12:40:31 +0100

    Create fetching reducer

---
+++ src/reducers/fetchingReducer.js
@@ -0,0 +1,18 @@
+import * as types from "../actions";
+const initialState = {
+  isFetching: true
+  // Array characters, Boolean fetching, null error.
+};
+export const fetchingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case (types.FETCHING):
+      console.log(action.payload)
+      return state;
+
+    // Fill me in with the important reducers
+    // action types should be FETCHING, SUCCESS and FAILURE
+    // your switch statement should handle all of these cases.
+    default:
+      return state;
+  }
+};
